5879659507291514c39850677e2ef6211c6e9c19
pupureum <pureuml.dev@gmail.com>
2023-07-08 21:32:13 +0900

    chore: querydsl 설정 추가

--- src/main/java/com/plee/library/LibraryApplication.java
+++ src/main/java/com/plee/library/LibraryApplication.java
@@ -3,13 +3,13 @@ package com.plee.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
 }
